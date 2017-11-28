377ebd1ed44db503f3d2b9f1dfcbb974e21a6bd9
Adam Walls <adam.walls@uky.edu>
2017-11-27 22:44:53 -0500

    Removed space from pom.xml

--- install_tip_client.sh
+++ install_tip_client.sh
@@ -1,2 +1,5 @@
 #!bin/bash/
-mvn install:install-file -Dfile=testInProgress-client-1.5-SNAPSHOT.jar -DgroupId=org.jenkins-ci.plugins -DartifactId=testInProgress-client -Dversion=1.5 -Dpackaging=jar
+# Call this from mystacktest directory
+# Assumes that there is a testInProgress-client-1.5-SNAPSHOT.jar
+# file in that directory
+mvn install:install-file -Dfile=./resources/testInProgress-client-1.5-SNAPSHOT.jar -DgroupId=org.jenkins-ci.plugins -DartifactId=testInProgress-client -Dversion=1.5 -Dpackaging=jar
